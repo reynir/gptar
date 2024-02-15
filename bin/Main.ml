03a57a1cef17eeb66b0d883dbe441c9c4b9093bd
Reynir Björnsson <reynir@reynir.dk>
2024-02-15 16:39:45 +0100

    Fix tar checksum, fix example binary
    When we change the tar link indicator after marshaling we need to update
    the tar header checksum. Thankfully, this is really easy.
    
    For the example binary:
    - some offsets is fixed,
    - we need to use Gpt.make,
    - copy the GPT header to the backup location (end of disk),
    - work around ocaml-tar bug

--- bin/Main.ml
+++ bin/Main.ml
@@ -5,13 +5,16 @@ let () =
   let oc = open_out_bin "disk.img" in
   let sector_size = Tar.Header.length in
   let disk_sectors = 1024L in
+  (* We create an empty partition table to figure out what would be the
+     first usable LBA. *)
   let empty =
-    Gpt.make ~sector_size ~disk_sectors []
+    Gpt.make ~current_lba:0L ~sector_size ~disk_sectors []
     |> Result.get_ok
   in
+  (* Partition names are utf16le encoded *)
   let name =
     let ascii_name = "Real tar archive" in
-    let buf = Bytes.create 72 in
+    let buf = Bytes.make 72 '\000' in
     String.iteri
       (fun i c ->
          ignore (Bytes.set_utf_16le_uchar buf (2*i) (Uchar.of_char c)))
@@ -25,18 +28,27 @@ let () =
       ~type_guid:tar_guid
       ~attributes:1L
       empty.first_usable_lba
-      (Int64.add empty.first_usable_lba 3L)
+      Int64.(pred (add empty.first_usable_lba 4L))
     |> Result.get_ok
   in
-  let gpt = { empty with Gpt.partitions = [ partition ] } in
+  let gpt =
+    Gpt.make ~current_lba:0L ~sector_size ~disk_sectors [ partition ]
+    |> Result.get_ok
+  in
   let buf = Cstruct.create (sector_size * Int64.to_int disk_sectors) in
+  (* We marshal the GPT+TAR header *)
   Gptar.marshal_header ~sector_size buf gpt;
+  (* Then the GPT partition table *)
   Gpt.marshal_partition_table ~sector_size 
     (Cstruct.shift buf sector_size) gpt;
+  (* Then we populate the partition with one "test.txt" file *)
   let content = "Hello, World!\n" in
   let tar_hdr = Tar.Header.make "test.txt" (Int64.of_int (String.length content)) in
-  let partition_buf = Cstruct.shift buf (sector_size * Int64.to_int gpt.first_usable_lba) in
-  Tar.Header.marshal partition_buf tar_hdr;
+  let partition_buf = Cstruct.shift buf (sector_size * Int64.(to_int partition.starting_lba)) in
+  (* XXX: there seems to be a bug in checksum computation in marshal *)
+  Tar.Header.marshal (Cstruct.sub partition_buf 0 Tar.Header.length) tar_hdr;
   Cstruct.blit_from_string content 0 partition_buf Tar.Header.length (String.length content);
+  (* Finally we copy the GPT+TAR header to the backup location (end of disk) *)
+  Cstruct.blit buf 0 buf (Cstruct.length buf - sector_size) sector_size;
   output_string oc (Cstruct.to_string buf);
   close_out oc
--- lib/Gptar.ml
+++ lib/Gptar.ml
@@ -2,7 +2,9 @@
 let magic_sequence = 0x2144df1cl
 let gpt_sizeof = 92 (* Gpt.sizeof *)
 let gpt_header_crc32_offset = 16
+let tar_chksum_offset = 148
 let tar_link_indicator_offset = 156
+let gptar_link_indicator = 'G'
 
 let marshal_header ~sector_size buf t =
   if Cstruct.length buf < sector_size ||
@@ -38,5 +40,15 @@ let marshal_header ~sector_size buf t =
      checksums. *)
   Tar.Header.marshal buf header;
   (* Let's fix up the link indicator so tar utilities like GNU tar will skip
-     the unknown type. The 'G' link indicator seems unused. *)
-  Cstruct.set_char buf tar_link_indicator_offset 'G'
+     the unknown type. The 'G' link indicator seems unused. We will need to
+     update the tar checksum then. *)
+  let old_link_indicator = Cstruct.get_uint8 buf tar_link_indicator_offset in
+  let old_checksum =
+    let s = Cstruct.to_string buf ~off:tar_chksum_offset ~len:8 in
+    let s = String.(trim (map (function '\000' -> ' ' | x -> x) s)) in
+    int_of_string ("0o"^s)
+  in
+  Cstruct.set_char buf tar_link_indicator_offset gptar_link_indicator;
+  (* The checksum is just a sum of the byte values in the header *)
+  let checksum = old_checksum - old_link_indicator + Char.code gptar_link_indicator in
+  Cstruct.blit_from_string checksum 0 buf tar_chksum_offset 8
